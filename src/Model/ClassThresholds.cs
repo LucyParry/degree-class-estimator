93ae0d934742d64b801908b4e5806e7aca8d2e8c
Lucy <lucy.parry@gmail.com>
2022-01-22 10:06:11 +0000

    Update Model and Test projects to .NET  6.0

--- src/Model/ClassThresholds.cs
+++ src/Model/ClassThresholds.cs
@@ -1,22 +1,15 @@
-﻿using Newtonsoft.Json;
-
-namespace HonoursClassEstimator.Model
+﻿namespace HonoursClassEstimator.Model
 {
     public class ClassThresholds : IResult
     {
-        [JsonProperty("availableCredit")]
         public int AvailableCredit { get; set; }
 
-        [JsonProperty("firstUpperBound")]
         public decimal FirstUpperBound { get; set; }
 
-        [JsonProperty("upperSecondUpperBound")]
         public decimal UpperSecondUpperBound { get; set; }
 
-        [JsonProperty("lowerSecondUpperBound")]
         public decimal LowerSecondUpperBound { get; set; }
 
-        [JsonProperty("thirdUpperBound")]
         public decimal ThirdUpperBound { get; set; }
     }
 }
