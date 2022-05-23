9a1e76737e2d6330540ec82a08084c2fab8295e5
Lucy <lucy.parry@gmail.com>
2022-05-23 22:44:07 +0100

    Rename method to get class attributes

--- src/Model/Utility/DataAttributeUtilities.cs
+++ src/Model/Utility/DataAttributeUtilities.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
-namespace Model.Utility
+namespace DegreeClassEstimator.Model
 {
     public static class DataAttributeUtilities
     {
-        public static Attr GetStringLengthAttribute<Type, Attr>(this Type aObject, string propertyName) where Attr: Attribute
+        public static Attr GetAttribute<Typ, Attr>(this Typ aObject, string propertyName) where Attr : Attribute
         {
-            Attr attribute = (Attr)aObject.GetType()
+            Attr attr = (Attr)aObject.GetType()
                 .GetProperties()
                 .SingleOrDefault(x => x.Name == propertyName)
                 .GetCustomAttributes(typeof(Attr), false)
                 .SingleOrDefault();
 
-            return attribute;
-}
+            return attr;
+        }
     }
 }
